76d8ebea928deabe96a5552f27223af7b2a1cf7e
SJH <289391777@qq.com>
2019-04-10 15:30:51 +0800

    add comment

--- MNIST/main.py
+++ MNIST/main.py
@@ -20,11 +20,11 @@ def train(p):
     train_step = tf.train.AdamOptimizer(learning_rate=0.008, beta2=0.99).minimize(loss)
 
 #   结果存放在一个布尔型列表中
-    correct_prediction = tf.equal(tf.argmax(ans, 1), tf.argmax(pred, 1))  # argmax返回一维张量中最大的值所在的位置
+    correct_prediction = tf.equal(tf.argmax(ans, 1), tf.argmax(pred, 1))  # argmax返回一维张量中最大的值所在的位置, 后面的1/0 表示按照哪个维度返回最大值。equal在相同的那个维度上是true，不同的维度上是false
 #   求准确率
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))  # cast转换数据类型，true转换成1, false转换成0, reduce_Mean不加参数则归一成标量，即算平均
 
-    saver = tf.train.Saver(max_to_keep=1)
+    saver = tf.train.Saver(max_to_keep=1)  # 始终保存当前训练的最新的模型（其他的删掉
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
@@ -35,7 +35,7 @@ def train(p):
                 _, acc = sess.run([train_step, accuracy], feed_dict={x: imgs, ans: labels})
                 print('[epoch {} step {}] acc {}'.format(j, i, acc))
 
-            saver.save(sess, 'ckpt/mnist.ckpt', global_step=j)
+            saver.save(sess, 'ckpt/mnist.ckpt', global_step=j)  # 会在模型的名字最后加上当前的epoch
 
 
 def test():
@@ -48,12 +48,11 @@ def test():
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
-        saver.restore(sess, './ckpt/mnist.ckpt-19')
-        for idx in range(100, 110):
+        saver.restore(sess, './ckpt/mnist.ckpt-20')
+        for idx in range(80, 100):
             img, label = mnist.train.images[idx], mnist.train.labels[idx]
             res = sess.run(pred, feed_dict={x: img.reshape(1, 784)})
-            print(np.argmax(res))
-            print(label)
+            print('Prediction is {0}; Correct answer is {1}'.format(np.argmax(res), np.argmax(label)))
 
             plt.imshow(img.reshape(28, 28), 'gray')
             plt.show()
@@ -65,4 +64,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     args.steps = 55000 // args.batch_size
 
-    train(args)
+    test()
